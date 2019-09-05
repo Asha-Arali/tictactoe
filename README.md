a01bf764770e90ffeeff4e2b65c72dd1c96802ce
Asha-Arali <ashu3377@gmail.com>
2019-09-04 21:46:29 -0500

    Tic tac toe game

---
+++ README.md
@@ -0,0 +1,4 @@
+# tictactoe
+Simple Spring Boot Tic Tac Toe game
+- Run the project (IntelliJ or STS)
+- Open Tic Tac Toe game in browser using http://localhost:8080/tictactoe/
---
+++ src/main/java/com/asha/tictactoe/Constants.java
@@ -0,0 +1,9 @@
+package com.asha.tictactoe;
+
+public class Constants {
+	public static final String GAME_STATE = "gameState";
+	public static final String VIEW_START = "start";
+	public static final String VIEW_PLAYER_SELECT = "playerselect";
+	public static final String VIEW_GAME = "tictactoe";
+	public static final String GAME_MODE = "AI_VS_HUMAN";
+}
